0f12eb97fe73c438f5dc488fb08ebfdd9607f11e
sei <vanism7557@gmail.com>
2023-02-05 17:58:26 +0900

    study: Lecture2 - create model
    - Card.swift
    - Concentration.swift

---
+++ Concentration/Concentration/Card.swift
@@ -0,0 +1,14 @@
+//
+//  Card.swift
+//  Concentration
+//
+//  Created by sei on 2023/02/05.
+//
+
+import Foundation
+
+struct Card {
+    var isFaceUp = false
+    var isMatched = false
+    var identifier: Int
+}
---
+++ Concentration/Concentration/Concentration.swift
@@ -0,0 +1,16 @@
+//
+//  Concentration.swift
+//  Concentration
+//
+//  Created by sei on 2023/02/05.
+//
+
+import Foundation
+
+class Concentration {
+    var cards = [Card]()
+
+    func chooseCard(at index: Int) {
+
+    }
+}
