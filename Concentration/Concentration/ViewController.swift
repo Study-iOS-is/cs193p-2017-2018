5264f0d46cccea8f5c30b68f373beb336ee16cae
sei <vanism7557@gmail.com>
2023-02-05 17:34:30 +0900

    Study: Lecture 1 - Concentration_1

---
+++ Concentration/Concentration/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by sei on 2023/02/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    private var flipCount = 0 {
+        didSet {
+            flipCountLabel.text = "Flips: \(flipCount)"
+        }
+    }
+
+    @IBOutlet weak var flipCountLabel: UILabel!
+
+    @IBOutlet var cardButtons: [UIButton]!
+
+    var emojiChoices = ["🎃", "👻", "🎃", "👻"]
+
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCount += 1
+        if let cardNumber = cardButtons.firstIndex(of: sender) {
+            flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
+        } else {
+            print("Chosen card was not in cardButtons")
+        }
+    }
+
+    func flipCard(withEmoji emoji: String, on button: UIButton) {
+        if button.currentTitle == emoji {
+            button.setTitle("", for: .normal)
+            button.backgroundColor = .orange
+        } else {
+            button.setTitle(emoji, for: .normal)
+            button.backgroundColor = .white
+        }
+    }
+
+}
+
