2b34a5a01000cec604db21dfea814a824df3a827
sei <vanism7557@gmail.com>
2023-02-06 23:18:40 +0900

    study: Lecture2. MVC

--- Concentration/Concentration/Concentration.swift
+++ Concentration/Concentration/Concentration.swift
@@ -9,8 +9,38 @@ import Foundation
 
 class Concentration {
     var cards = [Card]()
+    var indexOfOneAndOnlyFaceUpCard: Int?
+
+    init(numberOfPairsOfCards: Int) {
+        for _ in 0..<numberOfPairsOfCards {
+            let card = Card()
+            // struct 이므로 card는 copy됨
+            cards += [card, card]
+        }
+
+        // TODO: Shuffle the cards
+        cards.shuffle()
+    }
 
     func chooseCard(at index: Int) {
-
+        if !cards[index].isMatched {
+            if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index {
+                // check if cards match
+                if cards[matchIndex].identifier == cards[index].identifier {
+                    cards[index].isMatched = true
+                    cards[matchIndex].isMatched = true
+                }
+                // match가 아니라면, 다시 뒤집어야함.
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = nil
+            } else {
+                // either no cards or 2 cards are face up
+                for flipDownIndex in cards.indices {
+                    cards[flipDownIndex].isFaceUp = false
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = index
+            }
+        }
     }
 }
--- Concentration/Concentration/ViewController.swift
+++ Concentration/Concentration/ViewController.swift
@@ -8,37 +8,50 @@
 import UIKit
 
 class ViewController: UIViewController {
+    lazy var game = Concentration(numberOfPairsOfCards: (cardButtons.count/2))
 
-    private var flipCount = 0 {
-        didSet {
-            flipCountLabel.text = "Flips: \(flipCount)"
-        }
-    }
+    private var flipCount = 0 { didSet { flipCountLabel.text = "Flips: \(flipCount)" } }
 
     @IBOutlet weak var flipCountLabel: UILabel!
 
     @IBOutlet var cardButtons: [UIButton]!
 
-    var emojiChoices = ["🎃", "👻", "🎃", "👻"]
-
     @IBAction func touchCard(_ sender: UIButton) {
         flipCount += 1
         if let cardNumber = cardButtons.firstIndex(of: sender) {
-            flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
+            game.chooseCard(at: cardNumber)
+            updateViewFromModel()
         } else {
             print("Chosen card was not in cardButtons")
         }
     }
 
-    func flipCard(withEmoji emoji: String, on button: UIButton) {
-        if button.currentTitle == emoji {
-            button.setTitle("", for: .normal)
-            button.backgroundColor = .orange
-        } else {
-            button.setTitle(emoji, for: .normal)
-            button.backgroundColor = .white
+    func updateViewFromModel() {
+        for index in cardButtons.indices {
+            let button = cardButtons[index]
+            let card = game.cards[index]
+            if card.isFaceUp {
+                button.setTitle(emoji(for: card), for: .normal)
+                button.backgroundColor = .white
+            } else {
+                button.setTitle("", for: .normal)
+                button.backgroundColor = card.isMatched ? .clear : .orange
+            }
         }
     }
 
+
+    var emojiChoices = ["🎃", "👻", "🦇", "😱", "🙀", "😈", "🍭", "🍬", "🍎"]
+
+    var emoji = [Int: String]()
+
+    func emoji(for card: Card) -> String{
+        if emoji[card.identifier
+        ] == nil, emojiChoices.count > 0 {
+            let randomIndex = Int(arc4random_uniform(UInt32(emojiChoices.count)))
+            emoji[card.identifier] = emojiChoices.remove(at: randomIndex)
+        }
+        return emoji[card.identifier] ?? "?"
+    }
 }
 
